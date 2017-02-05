3fd401116b705b33d4845e9f56843addae6d6fcf
Ricardo Bánffy <rbanffy@gmail.com>
2017-02-05 22:30:55 +0000

    Add notice to content.js

--- content.js
+++ content.js
@@ -1,3 +1,20 @@
+// Replaces the image used for the frog face emoji with a more appropriate one.
+
+//     Copyright (C) 2017  Ricardo Bánffy
+
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 // Injected in the page, runs from there
 
 var PEPE_EMOJI = "url(\"https://abs.twimg.com/emoji/v2/72x72/1f438.png\")";
